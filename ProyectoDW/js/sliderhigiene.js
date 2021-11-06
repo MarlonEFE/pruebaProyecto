2e5039f21f264412f764dae564a29d2c6f2745f7
MarlonCabrera <88162254+MarlonEFE@users.noreply.github.com>
2021-11-06 00:49:32 -0500

    Add files via upload

---
+++ ProyectoDW/js/sliderhigiene.js
@@ -0,0 +1,19 @@
+var c=1;
+ var j=document.getElementById('js');
+function carrusel(){
+    j.style.opacity="0";
+    j.style.transition="all 1s";
+    setTimeout("cambio()",1000);
+}
+function cambio(){
+    c++;
+    if(c>3){
+        c=1;
+    }
+    j.setAttribute("src","../img/higiene/s"+c+".jpg");
+    j.style.opacity="1";
+    j.style.transition="all 1s";
+    setTimeout("carrusel()",2000);
+}
+
+document.body.setAttribute("onload","carrusel()");
